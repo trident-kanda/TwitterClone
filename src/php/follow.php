696d4d864887c4a3893431c35e8869091fe86ce2
trident-kanda <tridentkandayuki@gmail.com>
2021-05-26 17:49:57 +0900

    hide

--- src/php/follow.php
+++ src/php/follow.php
@@ -3,7 +3,7 @@ if(!empty($_POST["uid"])){
     $uid = $_POST["uid"];
     $followuid = $_POST["followuid"];
 try {
-    $dbh = new PDO('mysql:host=mysql148.phy.lolipop.lan;dbname=LAA1213637-twitter', "LAA1213637", "pass");
+    $dbh = new PDO('mysql:host=mysql148.phy.lolipop.lan;dbname=LAA1213637-twitter', "LAA1213637", "[password]");
     $sql = "INSERT INTO follow (uid,followuid) VALUES ('$uid','$followuid')";
     $res = $dbh->query($sql);
     echo json_encode("OK");
